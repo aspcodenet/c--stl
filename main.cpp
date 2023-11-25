c860875e6789807d017aaf70d62b2316374826ec
Stefan Holmberg <stefan.holmberg@systementor.se>
2023-11-25 14:28:28 +0100

    a

--- main.cpp
+++ main.cpp
@@ -10,7 +10,7 @@ public:
         MovieType_Film,
         MovieType_Tv
     }MovieType;
-
+    Movie(){}
     Movie(std::string name, int year,MovieType type, int price)
     :name(name),year(year),type(type),price(price){
     }
@@ -50,7 +50,66 @@ private:
 
 
 
+template<typename T>
+T findBiggest(T i, T j, T k){
+    if(i > j && i > k){
+        return i;
+    }
+    if(j > i && j > k){
+        return j;
+    }
+    return k;
+}
+
+template<typename T, int size>
+class MyArray{
+public:
+
+private:    
+    T things[size];
+};
+
+
+
+
 int main(){
+
+    MyArray<int,10> array;
+
+    MyArray<Movie,5> array2;
+    MyArray<std::string,5> array3;
+
+    MyArray<Movie,100> array4;
+    
+    int i= 19;
+    int j = 20;
+    int k = 21;
+
+    //int biggest = findBiggest<int>(i,j,k);
+    int biggest = findBiggest(i,j,k);
+    std::cout << biggest << std::endl;
+
+
+    float i2= 19.0;
+    float j2 = 20.0;
+    float k2 = 21.0;
+
+    float biggest2 = findBiggest(i2,j2,k2);
+    std::cout << biggest2 << std::endl;
+
+
+    std::string s1= "Stefan"; 
+    std::string s2= "Oliver"; 
+    std::string s3= "Josefine"; 
+
+    std::string stringBig = findBiggest(s1,s2,s3);
+    std::cout << stringBig << std::endl;
+
+
+
+    // templates = generics
+    // List<T>   ArrayList<T>
+
     std::vector<Movie> greatMovies{
         Movie("The Mummy returns",2001,Movie::MovieType::MovieType_Film,109),
         Movie("Fast and Furious 7",2014,Movie::MovieType::MovieType_Film,99),
@@ -62,16 +121,16 @@ int main(){
 
     //std::cin >> price;
     // I want to list all movies costing less than price
-    PriceFilter priceFilter(90);
-    auto i = std::begin(greatMovies),end = std::end(greatMovies);
-    while(i != end){
-        i = std::find_if(i,end,priceFilter);
-        // hello
-        if(i != end){
-            std::cout << i->getName() << std::endl;
-            i++;
-        }
-    }    
+    // PriceFilter priceFilter(90);
+    // auto i = std::begin(greatMovies),end = std::end(greatMovies);
+    // while(i != end){
+    //     i = std::find_if(i,end,priceFilter);
+    //     // hello
+    //     if(i != end){
+    //         std::cout << i->getName() << std::endl;
+    //         i++;
+    //     }
+    // }    
 
     // int price = 90;
     // //std::cin >> price;
